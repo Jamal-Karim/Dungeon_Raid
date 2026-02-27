3f2219755cbdb345745d22d7abf785eb0c8a8b23
Jamal-Karim <1jamalkarim@gmail.com>
2026-02-27 13:33:50 -0800

    Fix terminal UI bug

--- src/main/java/com/jamal_karim/dungeon/engine/Game.java
+++ src/main/java/com/jamal_karim/dungeon/engine/Game.java
@@ -24,26 +24,26 @@ public class Game {
     private final Scanner scanner = new Scanner(System.in);
     private final List<Entity> entitiesReadyToPlay = new ArrayList<>();
 
-    public void start(){
+    public void start() {
         boolean playing = true;
-        while(playing) {
+        while (playing) {
             setUpGame();
             setUpContext();
             playTurns();
 
             System.out.println("The raid is over! Play again? [1] Yes [2] No");
-            if(getValidChoice(2) == 2) playing = false;
+            if (getValidChoice(2) == 2) playing = false;
             else resetGame();
         }
     }
 
-    private void resetGame(){
+    private void resetGame() {
         entitiesReadyToPlay.clear();
         context = new BattleContext();
         turns = new TurnManager(context, ui);
     }
 
-    private void setUpGame(){
+    private void setUpGame() {
         System.out.println("\n================ Dungeon Raid ================");
 
         System.out.println("How many characters will be on each team? Max limit of 3");
@@ -58,9 +58,9 @@ public class Game {
 
         int choice = getValidChoice(2);
 
-        if(choice == 1){
+        if (choice == 1) {
             setUpUserTeam(numOfEntitiesPerTeam);
-        } else if(choice == 2){
+        } else if (choice == 2) {
             setUpCPUTeam(numOfEntitiesPerTeam);
         }
 
@@ -68,8 +68,8 @@ public class Game {
         scanner.nextLine();
     }
 
-    private void setUpContext(){
-        for(Entity e : entitiesReadyToPlay){
+    private void setUpContext() {
+        for (Entity e : entitiesReadyToPlay) {
             context.addToTeam(e);
         }
 
@@ -79,23 +79,23 @@ public class Game {
         scanner.nextLine();
     }
 
-    private void playTurns(){
+    private void playTurns() {
         turns.setOrderOfCharacters(context.getAllEntities());
         turns.playTurns();
     }
 
-    private void setUpCPUTeam(int numOfEntities){
+    private void setUpCPUTeam(int numOfEntities) {
 
         Random random = new Random();
         int threeDigitInt = random.nextInt(900) + 100;
 
         String teamName = "Monsters_" + threeDigitInt;
 
-        for(int i = 0; i < numOfEntities; i++){
+        for (int i = 0; i < numOfEntities; i++) {
             int randomEntityChoice = ThreadLocalRandom.current().nextInt(1, 4);
             int entitySuffix = random.nextInt(900) + 100;
 
-            switch (randomEntityChoice){
+            switch (randomEntityChoice) {
                 case 1:
                     Entity warrior = new Warrior("Warrior_" + entitySuffix, teamName);
                     warrior.setController(WarriorCPU);
@@ -119,12 +119,12 @@ public class Game {
 
     }
 
-    private void setUpUserTeam(int numOfEntities){
+    private void setUpUserTeam(int numOfEntities) {
         System.out.println("Choose your team name");
         String teamName = scanner.nextLine();
 
-        for(int i = 0; i < numOfEntities; i++){
-            System.out.println("What is the first entity you want to recruit:");
+        for (int i = 0; i < numOfEntities; i++) {
+            System.out.println("What is the entity you want to recruit:");
             System.out.println("[1]: Warrior");
             System.out.println("[2]: Mage");
             System.out.println("[3]: Tank");
@@ -132,7 +132,7 @@ public class Game {
             int choice = getValidChoice(3);
             String entityName;
 
-            switch (choice){
+            switch (choice) {
                 case 1:
                     System.out.println("Give your Warrior a name");
                     entityName = scanner.nextLine();
@@ -147,7 +147,7 @@ public class Game {
                     Entity mage = new Mage(entityName, teamName);
                     mage.setController(Player);
                     entitiesReadyToPlay.add(mage);
-                    System.out.printf("%s enters the raid with %s\n", mage.getName(),teamName);
+                    System.out.printf("%s enters the raid with %s\n", mage.getName(), teamName);
                     break;
                 case 3:
                     System.out.println("Give your Tank a name");
@@ -166,7 +166,8 @@ public class Game {
             try {
                 int choice = Integer.parseInt(scanner.nextLine());
                 if (choice >= 1 && choice <= max) return choice;
-            } catch (NumberFormatException e) { }
+            } catch (NumberFormatException e) {
+            }
             System.out.println("Not a valid choice. Try again.");
         }
     }
